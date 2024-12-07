b9541cfb5a4ecf3a16e5896eecf7e0510e8c7ea3
DESKTOP-DNTA4BT\lokati <reedus.chung@gmail.com>
2024-12-08 00:24:32 +0900

    no message

--- Exercises/20245360_CHUNG_QUANG_KHANH_HW#8/project_4/compute_44.m
+++
@@ -1,9 +0,0 @@
-function [ Omega ] = compute_44( omega )
-
-Omega = [ 0 , -omega(1) , -omega(2) , -omega(3) ; ...
-          omega(1) , 0 , omega(3) , -omega(2) ; ...
-          omega(2) , -omega(3) , 0 , omega(1) ; ...
-          omega(3) , omega(2) , -omega(1) , 0];
-
-end
-
